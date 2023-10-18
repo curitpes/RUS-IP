6866f7282d31914d87d181d90a9d665e1c3d7ffb
curitpes <148391976+curitpes@users.noreply.github.com>
2023-10-18 23:56:42 +0300

    main.py

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import requests
 from pyfiglet import Figlet
-import folium
 
 
 def get_info_by_ip(ip='127.0.0.1'):
@@ -22,9 +21,6 @@ def get_info_by_ip(ip='127.0.0.1'):
 
         for k, v in data.items():
             print(f'{k} : {v}')
-
-        area = folium.Map(location=[response.get('lat'), response.get('lon')])
-        area.save(f'{response.get("query")}_{response.get("city")}.html')
 
     except requests.exceptions.ConnectionError:
         print('[!] Please check your connection!')
